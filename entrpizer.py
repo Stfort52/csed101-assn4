20ceb12c0b79cf916b4ac6eabd9e870e9df73f88
Stfort52 <52439950+Stfort52@users.noreply.github.com>
2019-11-25 20:22:52 +0900

    Now 2.1% more tidy

--- entrpizer.py
+++ entrpizer.py
@@ -2,7 +2,6 @@ from PIL import Image
 
 def entropize(image, rdiff = (0,0), gdiff = (0,0)):
     r,g,b = image.split()
-    boundry = (0, 0) + image.size
     width, height = image.size #l u r d
     bedge   = (max(rdiff[0], gdiff[0], 0), max(rdiff[1], gdiff[1], 0), width + min(rdiff[0], gdiff[0], 0), height + min(rdiff[1], gdiff[1], 0))
     redge   = (max(0, -1 * rdiff[0], gdiff[0] - rdiff[0]), max(0, -1 * rdiff[1], gdiff[1] - rdiff[1]), min(width, width - rdiff[0], width + gdiff[0] -rdiff[0]), min(height, height - rdiff[1], height + gdiff[1] -rdiff[1]))
